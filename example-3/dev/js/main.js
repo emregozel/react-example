e6f9b1125d5fd41a9f79a0abcc90c3890f99f844
Emre Gözel <emregozel@Emre-MacBook-Pro.local>
2016-11-19 18:02:06 +0300

    Example 3 added
    React JS example 3 facebook login add.

---
+++ example-3/dev/js/main.js
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import FacebookButton from "./components/FacebookButton";
+
+ReactDOM.render(
+  <FacebookButton fb={FB} />
+  , document.getElementById('app'));
---
+++ example-3/webpack.config.js
@@ -0,0 +1,35 @@
+var webpack = require('webpack');
+
+module.exports = {
+
+    entry: './dev/js/main.js',
+    devServer: {
+        inline: true,
+        contentBase: './src',
+        port: 3000
+    },
+    module: {
+      preLoaders: [
+      { test: /\.json$/, exclude: /node_modules/, loader: 'json'},
+  ],
+        loaders : [
+            {
+                test: /\.js$/,
+                exclude: /(node_modules)/,
+                loader: 'babel',
+                query: {
+                    presets: ['react', 'es2015']
+                }
+            },
+            {
+                test: /\.scss/,
+                loader: 'style-loader!css-loader!sass-loader'
+            }
+        ]
+    },
+    devtool: 'cheap-module-eval-source-map',
+    output: {
+        path: 'src',
+        filename: 'js/bundle.min.js'
+    }
+};
